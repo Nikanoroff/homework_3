ed64607156cd7b448f7ac03aacb9f0c1a2163f0c
Nikanorov_Ivan <patissonov@icloud.com>
2021-10-14 22:11:28 +0300

    added homework_3

---
+++ script.js
@@ -0,0 +1,74 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const GOODS = '/catalogData.json';
+
+class GoodsItem {
+    constructor(img = "img", title = "title", price = 0, desc = "some text") {
+        this.img = img;
+        this.title = title;
+        this.price = price;
+        this.desc = desc;
+    }
+    render() {
+        return `
+    <div class="goods-item">
+    <img scr=${this.img} width="250" height="250"/>
+    <h3>${this.title}</h3>
+    <p class="goods_price">${this.price}</p>
+    <p class="goods_desc">${this.desc}</p>
+    </div>
+    `;
+    }
+}
+
+
+class GoodsList {
+    goods = [];
+    constructor() {
+        this.fetchGoods(() => {
+            this.render();
+        });
+
+    }
+
+
+    fetchGoods(callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `${API}${GOODS}`, true);
+        xhr.send();
+        xhr.onload = () => {
+            this.goods = JSON.parse(xhr.responseText);
+            callback();
+        }
+
+
+    }
+
+    // getCount() {
+    //     let count = 0;
+    //     this.goods.forEach(({ price }) => {
+    //         count += price;
+    //     })
+    //     return count;
+    // }
+
+    render() {
+        const goodsList = this.goods.map(({ img, product_name, price, desc }) => {
+            const goodsItem = new GoodsItem(img, product_name, price, desc);
+            return goodsItem.render()
+        });
+        document.querySelector('.goods-list').innerHTML = goodsList.join('');
+    }
+}
+
+
+
+
+window.onload = () => {
+    const goodsList = new GoodsList();
+
+}
+
+
+
+
+
